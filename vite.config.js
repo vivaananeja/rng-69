dd3f7882cdfd8ca3c4cc7ac45064c77eaf8d62e9
vivaananeja <vivaananeja@gmail.com>
2025-08-09 22:51:44 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// IMPORTANT: change 'rng69' below to EXACTLY your repo name
-export default defineConfig({
-  plugins: [react()],
-  base: "/rng69/"
-});
